fe2421012f4db7d93f57ca1b61607e7f4be2acda
victorizbitskiy <victor.izbitskiy@yandex.ru>
2023-02-24 17:15:52 +0300

    add font burtons

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
       <main className="bg-white px-10">
         <section className="min-h-screen">
           <nav className="py-10 mb-12 flex justify-between">
-            <h1 className="text-xl">developedbyme</h1>
+            <h1 className="text-xl font-burtons">developedbyme</h1>
             <ul className="flex items-center">
               <li>
                 <BsFillMoonStarsFill className="cursor-pointer text-2xl" />
