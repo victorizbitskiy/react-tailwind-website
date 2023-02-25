18eb9691e482343568e7d15c56c1b7c833e81bf8
victorizbitskiy <victor.izbitskiy@yandex.ru>
2023-02-25 10:30:41 +0300

    add image

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import { BsFillMoonStarsFill } from "react-icons/bs";
 import {AiFillGithub, AiFillLinkedin} from "react-icons/ai";
+import devme from '../public/assets/devme.png'
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -41,6 +43,9 @@ export default function Home() {
           <div className="text-5xl flex justify-center gap-5 py-3 text-gray-600">
             <AiFillGithub />
             <AiFillLinkedin />
+          </div>
+          <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden">
+            <Image src={devme} fill="true" style={{objectFit: "cover"}} alt="devme"/>
           </div>
         </section>
       </main>
