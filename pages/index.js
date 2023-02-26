417d7ac5ae21a3daa5e667656f5f45719e4ed6f0
victorizbitskiy <victor.izbitskiy@yandex.ru>
2023-02-26 17:34:48 +0300

    add a few paragraphs with about descr

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import { BsFillMoonStarsFill } from "react-icons/bs";
-import {AiFillGithub, AiFillLinkedin} from "react-icons/ai";
-import devme from '../public/assets/devme.png'
+import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
+import devme from "../public/assets/devme.png";
 import Image from "next/image";
 
 export default function Home() {
@@ -33,11 +33,13 @@ export default function Home() {
             </ul>
           </nav>
           <div className="text-center p-10">
-            <h2 className="text-5xl py-2 text-teal-500 font-medium">Victor Izbitskiy</h2>
+            <h2 className="text-5xl py-2 text-teal-500 font-medium">
+              Victor Izbitskiy
+            </h2>
             <h3 className="text-2xl py-2">Developer and designer.</h3>
             <p className="text-md py-5 leading-8 text-gray-800">
               Freelancer providing services for programming and design content
-              needs. Join me down below and let`s get cracking! 
+              needs. Join me down below and let`s get cracking!
             </p>
           </div>
           <div className="text-5xl flex justify-center gap-5 py-3 text-gray-600">
@@ -45,7 +47,30 @@ export default function Home() {
             <AiFillLinkedin />
           </div>
           <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden">
-            <Image src={devme} fill="true" style={{objectFit: "cover"}} alt="devme"/>
+            <Image
+              src={devme}
+              fill="true"
+              style={{ objectFit: "cover" }}
+              alt="devme"
+            />
+          </div>
+        </section>
+
+        <section>
+          <div>
+            <h3 className="text-3xl py-1">Services I offer</h3>
+            <p className="text-md py-2 leading-8 text-gray-800">
+              Since the beginning of my journey as a freelance designer and
+              developer, I`ve done remote work for
+              <span className="text-teal-500"> agencies</span> consulted for{" "}
+              <span className="text-teal-500"> startups</span> and collaborated
+              with talented people to create digital products for both business
+              and consumer use.
+            </p>
+            <p className="text-md py-2 leading-8 text-gray-800">
+              I offer from a wide range of services, including brand desing?
+              programming and teaching.
+            </p>
           </div>
         </section>
       </main>
